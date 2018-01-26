e3e2a70c8e688e30a1fda474d6499e77337312fb
Jiyang Wang <jiyang.wang.us@gmail.com>
2018-01-25 23:07:22 -0800

    Jan.25, 2018

---
+++ timer.js
@@ -0,0 +1,15 @@
+function count(start, end) {
+  console.log(start++);
+  var timer = setInterval(function() {
+      if (start <= end) {
+          console.log(start++);
+      } else {
+          clearInterval(timer);
+      }   
+  }, 100);
+  return {
+      cancel: function() {
+          clearInterval(timer);
+      }
+  }
+}
