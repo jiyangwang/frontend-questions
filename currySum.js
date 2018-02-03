faa38138be40b51975f90e9464e3c56aee56ac97
Jiyang Wang <jiyang.wang.us@gmail.com>
2018-02-02 22:32:47 -0800

    Feb. 2

---
+++ currySum.js
@@ -0,0 +1,14 @@
+var sum = function(x) {
+  var args = Array.prototype.slice.call(arguments);
+  if (args.length > 1) {
+    return args.reduce(function(acc, cur) {
+      return acc + cur;  
+    });
+  }
+  return function(y) {
+    return x + y;  
+  }
+}
+ 
+console.log(sum(1, 2));
+console.log(sum(1)(2));
