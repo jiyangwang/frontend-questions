e32c0bf91a52efe89fb4d83f175c729bb9713273
Jiyang Wang <jiyang.wang.us@gmail.com>
2018-02-16 23:02:00 -0800

    Feb.16, 2018

--- clone.js
+++ clone.js
@@ -1,29 +1,30 @@
-// 方法一：
-Object.prototype.clone = function(){
-  var o = this.constructor === Array ? [] : {};
-  for(var e in this){
-          o[e] = typeof this[e] === "object" ? this[e].clone() : this[e];
-  }
-  return o;
+Object.prototype.clone = function () {
+    var o = this.constructor === Array ? [] : {};
+    for (var e in this) {
+        o[e] = typeof this[e] === "object" ? this[e].clone() : this[e];
+    }
+    return o;
 }
 
-//方法二：
 function clone(Obj) {
-  var buf;
-  if (Obj instanceof Array) {
-      buf = [];                    //创建一个空的数组 
-      var i = Obj.length;
-      while (i--) {
-          buf[i] = clone(Obj[i]);
-      }
-      return buf;
-  }else if (Obj instanceof Object){
-      buf = {};                   //创建一个空对象 
-      for (var k in Obj) {           //为这个对象添加新的属性 
-          buf[k] = clone(Obj[k]);
-      }
-      return buf;
-  }else{                         //普通变量直接赋值
-      return Obj;
-  }
+    var buf;
+    if (Obj instanceof Array) {
+        buf = [];
+        var i = Obj.length;
+        while (i--) {
+            buf[i] = clone(Obj[i]);
+        }
+        return buf;
+    } else if (Obj instanceof Object) {
+        buf = {};
+        for (var k in Obj) {
+            buf[k] = clone(Obj[k]);
+        }
+        return buf;
+    } else {
+        return Obj;
+    }
 }
+
+var obj = { arr: [1,2,3,4,5], a: 1, b: 2 }
+console.log(clone(obj));
